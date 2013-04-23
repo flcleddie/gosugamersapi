3223768ac6ed9f301f962748f786fcce9f49ee73
Eddie <flcleddie@ubuntu.(none)>
2013-04-22 20:06:00 -0700

    initial commit

---
+++ gosugamersapi/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for gosugamersapi project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'gosugamersapi'
+
+SPIDER_MODULES = ['gosugamersapi.spiders']
+NEWSPIDER_MODULE = 'gosugamersapi.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'gosugamersapi (+http://www.yourdomain.com)'
