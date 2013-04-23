94a7d93e0440093c7890da332fe67bc7a60b0156
Eddie Lee <eddie.d.lee@gmail.com>
2013-04-22 23:12:37 -0700

    saving to db

--- gosugamersapi/pipelines.py
+++ gosugamersapi/pipelines.py
@@ -1,8 +1,15 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/topics/item-pipeline.html
+import pymongo
+
+from scrapy.exceptions import DropItem
+from scrapy.conf import settings
+from scrapy import log
 
 class GosugamersapiPipeline(object):
+    def __init__(self):
+        connection = pymongo.MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
     def process_item(self, item, spider):
+        self.collection.insert(dict(item))
         return item
--- gosugamersapi/settings.py
+++ gosugamersapi/settings.py
@@ -1,15 +1,12 @@
-# Scrapy settings for gosugamersapi project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/topics/settings.html
-#
 
 BOT_NAME = 'gosugamersapi'
 
 SPIDER_MODULES = ['gosugamersapi.spiders']
 NEWSPIDER_MODULE = 'gosugamersapi.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'gosugamersapi (+http://www.yourdomain.com)'
+ITEM_PIPELINES = ['gosugamersapi.pipelines.GosugamersapiPipeline',]
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "gosugamers"
+MONGODB_COLLECTION = "matches"
