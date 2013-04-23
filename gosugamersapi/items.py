c84587b9a7312af969828fbd57d5a892352b9640
Eddie <flcleddie@ubuntu.(none)>
2013-04-22 20:44:52 -0700

    added item fields

--- gosugamersapi/items.py
+++ gosugamersapi/items.py
@@ -1,11 +1,10 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/topics/items.html
-
 from scrapy.item import Item, Field
 
 class GosugamersapiItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+    game = Field()
+    playerOne = Field()
+    playerOneCountry = Field()
+    playerTwo = Field()
+    playerTwoCountry = Field()
+    tournament = Field()
+    time = Field()
